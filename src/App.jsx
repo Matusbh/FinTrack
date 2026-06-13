b2f3dcf6ed2cdfbb27598b2626e4d17287a408cc
Matusbh <matusbehun03@gmail.com>
2026-06-13 18:11:49 +0100

    Creadas las rutas de las transacciones e indexada la ruta por defecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import Dashboard from "./pages/DashboardPage.jsx";
-
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout.jsx";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import TransactionsPage from "./pages/TransactionsPage.jsx";
 function App() {
-  return <Dashboard />;
+  return (
+    <Layout>
+      <Routes>
+        {
+          //ruta por defecto la que esta indexada
+        }
+        <Route index element={<DashboardPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/trnasactions" element={<TransactionsPage />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
--- src/pages/TransactionsPage.jsx
+++ src/pages/TransactionsPage.jsx
@@ -1,7 +1,17 @@
 // Página de transacciones. Muestra la lista completa y permite crear, editar y eliminar.
+
+// Página principal de la app. Carga y muestra las SummaryCards y los gráficos de gastos.
+import Header from "../components/layout/Header.jsx";
+import { Plus } from "lucide-react";
+import Card from "../components/ui/Card.jsx";
+
 export default function TransactionsPage() {
   return (
     <>
+      <div>
+        <Header />
+      </div>
+
       <h1
         className="text-2xl font-semibold text-[#1A1A1A] tracking-tight
             "
